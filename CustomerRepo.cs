a0edb386427c3ca51e64b4d21331c0ed8dd98afc
AimanSiddiqua696 <siddiquaaimen@gmail.com>
2025-12-06 13:33:10 +0500

    Add files via upload

---
+++ CustomerRepo.cs
@@ -0,0 +1,170 @@
+﻿using practiceproject.Product;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace database.Customer
+{
+    internal class CustomerRepo
+    {
+        private readonly string file = @"C:\Users\User\Desktop\AimanSiddiqua\customers.txt";
+        public CustomerRepo()
+        {
+
+        }
+        public void SaveInFile(CustomerModel customer) //save the data for a single object in a file
+        {
+            using (StreamWriter stream = new StreamWriter(file, true))
+            {
+                stream.WriteLine(customer.ToString());
+
+            }
+
+        }
+        public void SaveAllDataIntoFile(List<CustomerModel> customers)
+        {
+            //this code of lines also delete all the data from the file
+            //means empty the file first and then foreach loop will run
+            //using (StreamWriter stream = new StreamWriter(file, false))
+            //{
+            //    stream.Write(file, "");
+            //        }
+            //or instead of writing above code to empty the file we can only write a single line of code which is more good
+            File.WriteAllText(file, ""); //this line delete all the data from the file
+            foreach (CustomerModel customer in customers)
+            {
+                SaveInFile(customer);
+            }
+        }
+
+        public List<CustomerModel> GetCustomersFromFile() //read the all data from the file and load into the list and return the list
+                                                          //when we need all the data from the file , we call the function it will
+                                                          //create a list load  all the data from the file into a list and return you a list 
+        {
+            List<CustomerModel> customersLists = new List<CustomerModel>();
+            using (StreamReader stream = new StreamReader(file))
+            {
+                string line = "";
+                while ((line = stream.ReadLine()) != null)
+                {
+                    if (line.Length > 5)
+                    {
+                        string[] parts = line.Split(',');
+                        string customerName = parts[0];
+                        string phonenumber = parts[1];
+                        int age = int.Parse(parts[2]);
+                        string address = parts[3];
+
+
+                        //now we will create an object and then you will add this object to this list 
+                        CustomerModel customerModel = new CustomerModel(customerName, phonenumber, age, address);
+                        customersLists.Add(customerModel);
+                    }
+
+
+                }
+            }
+            return customersLists;
+        }
+        public readonly string DbConnection = "Server=LocalHost;Database=PointOfSale;Trusted_Connection=True";
+        public bool Create(CustomerModel Customer)
+        {
+            using (SqlConnection conn = new SqlConnection(DbConnection))
+            {
+                string query = "INSERT INTO CUSTOMER1 (cname, phonenumber, age, address)" +
+                    "VALUES (@cname, @phonenumber, @age, @address)";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@cname", Customer.cname);
+                cmd.Parameters.AddWithValue("@phonenumber", Customer.phonenumber);
+                cmd.Parameters.AddWithValue("@age", Customer.age);
+                cmd.Parameters.AddWithValue("@address", Customer.address);
+
+                conn.Open();
+                int rowsAffected = cmd.ExecuteNonQuery();
+                if (rowsAffected > 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+        public bool Update(CustomerModel customer)
+        {
+            using (SqlConnection conn = new SqlConnection(DbConnection))
+            {
+                string query = "UPDATE CUSTOMER1 SET cname= @cname, phonenumber=@phonenumber, age=@age, address=@address WHERE Id = @Id";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@Id", customer.id);
+                cmd.Parameters.AddWithValue("@cname", customer.cname);
+                cmd.Parameters.AddWithValue("@phonenumber", customer.phonenumber);
+                cmd.Parameters.AddWithValue("@age", customer.age);
+                cmd.Parameters.AddWithValue("@address", customer.address);
+                conn.Open();
+                int rowsAffected = cmd.ExecuteNonQuery();
+                if (rowsAffected > 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+        public bool Delete(int id)
+        {
+            using (SqlConnection conn = new SqlConnection(DbConnection))
+            {
+                string query = "DELETE FROM CUSTOMER1 WHERE Id = @Id";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@Id", id);
+                conn.Open();
+                int rowsAffected = cmd.ExecuteNonQuery();
+                if (rowsAffected > 0)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+        public List<CustomerModel> GetAll()
+        {
+            List<CustomerModel> customers = new List<CustomerModel>();
+            using (SqlConnection conn = new SqlConnection(DbConnection))
+            {
+                string query = "SELECT id, cname, phonenumber, age, address FROM CUSTOMER1";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                conn.Open();
+                SqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    int id = Convert.ToInt32(reader["id"]);
+                    string cname = reader["cname"].ToString();
+                    string phonenumber = reader["phonenumber"].ToString();
+                    int age = Convert.ToInt32(reader["age"]);
+                    string address = reader["address"].ToString();
+                  
+                    CustomerModel product = new CustomerModel(id, cname, phonenumber, age, address);
+                    customers.Add(product);
+                }
+                reader.Close();
+            }
+            return customers;
+
+        }
+    }
+}
+
+        
+
+    
---
+++ CustomerUI.cs
@@ -0,0 +1,343 @@
+﻿using database.Product;
+using practiceproject.Product;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace database.Customer
+{
+    internal class CustomerUI
+    {
+
+        CustomerService service = new CustomerService();
+        public void CustomerDriver()
+        {
+            while (true)
+            {
+                Console.Clear();
+                string option = CustomerMenu();
+                if (option == "1")
+                {
+                    //CustomerModel customer = TakeInput();
+                    //service.SaveCustomer(customer);
+                    AddCustomerToDatabase();
+                }
+                else if (option == "2")
+                {
+                    //UpdateCustomer();
+                    UpdateCustomerInDatabase(); 
+                }
+                else if (option == "3")
+                {
+                    //DeleteCustomer();
+                    DeleteCustomerToDatabase();
+                }
+                else if (option == "4")
+                {
+                    //DisplayAll();
+                    GetAllCustomerFromDatabase();
+                }
+                else if (option == "5")
+                {
+                    while (true)
+                    {
+                        Console.Clear();
+                        string Option = AdvanceSearch();
+                        if (Option == "1")
+                        {
+                            SearchByNameCustomer();
+                        }
+                        if (Option == "2")
+                        {
+                            SearchBYFirstCharacter();
+                        }
+                        if (Option == "3")
+                        {
+                            SearchBYNumber();
+                        }
+                        if (Option == "4")
+                        {
+                            SearchByAddress();
+                        }
+                        if (Option == "5")
+                        {
+
+                        }
+                        if (Option == "6")
+                        {
+                            break;
+                        }
+                    }
+                }
+                else if (option == "6")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Wrong option selected!");
+                }
+            }
+        }
+        public void UpdateCustomer()
+        {
+            Console.WriteLine("Enter customer name");
+            string cname = Console.ReadLine();
+            Console.WriteLine("Enter customer phonenumber");
+            string phonenumber = Console.ReadLine();
+            service.UpdateCustomer(cname, phonenumber);
+        }
+        public void DeleteCustomer()
+        {
+            Console.WriteLine("Enter customer name");
+            string cname = Console.ReadLine();
+
+            service.DeleteCustomer(cname);
+        }
+        public void DisplayAll()
+        {
+            List<CustomerModel> customers = service.GetAllData();
+            foreach (CustomerModel c in customers)
+            {
+                Console.WriteLine(c.ToString());
+            }
+            Console.ReadKey();
+        }
+        public void SearchByNameCustomer()
+        {
+            Console.WriteLine("Enter customer name ");
+            string name = Console.ReadLine();
+            List<CustomerModel> filter = service.SearchCustomerByName(name);
+            if (filter.Count > 0)
+            {
+                foreach (CustomerModel c in filter)
+                {
+                    Console.WriteLine(c.ToString());
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Customer Not Found!");
+            }
+            Console.ReadKey();
+
+        }
+        public void SearchBYFirstCharacter()
+        {
+            Console.WriteLine("Enter customer first character ");
+            char letter = char.Parse(Console.ReadLine());
+            List<CustomerModel> filter = service.SearchCutomerByFirstName(letter);
+            if (filter.Count > 0)
+            {
+                foreach (CustomerModel c in filter)
+                {
+                    Console.WriteLine(c.ToString());
+                }
+            }
+            else
+            {
+                Console.WriteLine("Customer Not Found!");
+            }
+            Console.ReadKey();
+
+        }
+        public void SearchBYNumber()
+        {
+            Console.WriteLine("Enter customer phone number ");
+            string number = Console.ReadLine();
+            List<CustomerModel> filter = service.SearchCutomerByPhoneNumber(number);
+            if (filter.Count > 0)
+            {
+                foreach (CustomerModel c in filter)
+                {
+                    Console.WriteLine(c.ToString());
+                }
+            }
+            else
+            {
+                Console.WriteLine("Phone Number Not Found!");
+            }
+            Console.ReadKey();
+
+        }
+        public void SearchByAddress()
+        {
+            Console.WriteLine("Enter customer address ");
+            string address = Console.ReadLine();
+            List<CustomerModel> adrs = service.SearchCustomerByAddress(address);
+            if (adrs.Count > 0)
+            {
+                foreach (CustomerModel ad in adrs)
+                {
+                    Console.WriteLine(ad.ToString());
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid Address!");
+            }
+            Console.ReadKey();
+        }
+        public void AddCustomerToDatabase()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("Enter customer name");
+            string cname = Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("Enter customer phonenumber");
+            string phonenumber = Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("Enter customer age");
+            int age = int.Parse(Console.ReadLine());
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.WriteLine("Enter customer address");
+            string address = Console.ReadLine();
+            CustomerModel c = new CustomerModel(cname, phonenumber, age, address);
+            CustomerRepo repo = new CustomerRepo();
+            bool result = repo.Create(c);
+            if (result)
+            {
+                Console.WriteLine("Customer Added");
+
+            }
+            else
+            {
+                Console.WriteLine("Failed to add customer");
+            }
+            Console.ReadKey();
+        }
+        public void UpdateCustomerInDatabase()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Enter Id of the customer");
+            int id = int.Parse(Console.ReadLine());
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Enter customer name");
+            string cname = Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Enter customer phonenumber");
+            string phonenumber = Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Enter customer age");
+            int age = int.Parse(Console.ReadLine());
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("Enter customer address");
+            string address = Console.ReadLine();
+            CustomerModel c = new CustomerModel(id,cname, phonenumber, age, address);
+            CustomerRepo repo = new CustomerRepo();
+            bool result = repo.Update(c);
+            if (result)
+            {
+                Console.WriteLine("Customer Updated");
+
+            }
+            else
+            {
+                Console.WriteLine("Failed to update customer");
+            }
+            Console.ReadKey();
+        }
+        public void DeleteCustomerToDatabase()
+        {
+            Console.WriteLine("Enter id of the customer");
+            int id = int.Parse(Console.ReadLine());
+           CustomerRepo repo = new CustomerRepo();
+            bool result = repo.Delete(id);
+            if (result)
+            {
+                Console.WriteLine("Customer Deleted");
+            }
+            else
+            {
+                Console.WriteLine("Failed to delete customer");
+            }
+            Console.ReadKey();
+        }
+        public void GetAllCustomerFromDatabase()
+        {
+
+            CustomerRepo repo = new CustomerRepo();
+            List<CustomerModel> customers = repo.GetAll();
+            foreach (CustomerModel p in customers)
+            {
+                Console.WriteLine($"Id:{p.id}");
+                Console.WriteLine($"Name:{p.cname}");
+                Console.WriteLine($"Phonenumber:{p.phonenumber}");
+                Console.WriteLine($"Age:{p.age}");
+                Console.WriteLine($"Address:{p.address}");
+            }
+            Console.ReadKey();
+
+        }
+
+        public CustomerModel TakeInput()
+        {
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Enter customer name");
+            string cname = Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Enter customer phonenumber");
+            string phonenumber = Console.ReadLine();
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Enter customer age");
+            int age = int.Parse(Console.ReadLine());
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Enter customer address");
+            string address = Console.ReadLine();
+
+            //to make the process fast first we create the object and store the data
+            //in the object and then we return the object
+            CustomerModel customer = new CustomerModel(cname, phonenumber, age, address);
+            return customer;
+        }
+        public string CustomerMenu()
+
+        {
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine("------------------------------");
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine("       Customer Management     ");
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine("------------------------------");
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("1 Add ");
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("2 Update ");
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("3 Delete ");
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("4 View All ");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("5 Advance Search ");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("6 Back");
+            Console.ForegroundColor = ConsoleColor.White;
+            string option = Console.ReadLine();
+            return option;
+
+        }
+        public string AdvanceSearch()
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("1 Find Customer by name ");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("2 Find Customer by First Character  ");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("3 Find Customer by PhoneNumber ");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("4 Find Customer by Address ");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("5 Find Customer by Age ");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("6 Back ");
+            Console.WriteLine("Enter your option ");
+            string Option = Console.ReadLine();
+            return Option;
+
+        }
+
+    }
+}
